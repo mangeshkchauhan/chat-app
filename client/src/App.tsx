90e38304969461e8a45357564a92edaa9620afb5
Mangesh <63477318+mangeshkchauhan@users.noreply.github.com>
2023-02-15 19:08:22 +0530

    Feat: added access functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,23 +15,33 @@ const socket = io('https://chat-app-uptut.onrender.com', {
 function App() {
   const [username, setUsername] = useState('');
   const [room, setRoom] = useState('');
+  const [user, setUser] = useState<{ name: string } | undefined>();
   const [showChat, setShowChat] = useState(false);
 
   const joinRoom = () => {
     if (username !== '' && room !== '') {
       socket.emit('join_room', room);
+      setUser({ name: username });
       setShowChat(true);
+    } else {
+      alert('Enter username and roomId you want to connect to');
     }
   };
 
-  const onLogin = () => {};
-  const onLogout = () => {};
-  const onCreateAccount = () => {};
+  const onLogin = () => {
+    joinRoom();
+  };
+  const onLogout = () => {
+    window.location.reload();
+  };
+  const onCreateAccount = () => {
+    alert('I am sorry, This feature is under devdelopment');
+  };
 
   return (
     <Provider store={store}>
       <Header
-        user={{ name: username }}
+        user={user}
         onLogin={onLogin}
         onLogout={onLogout}
         onCreateAccount={onCreateAccount}
@@ -42,7 +52,7 @@ function App() {
             <h3>Join A Chat</h3>
             <input
               type='text'
-              placeholder='John...'
+              placeholder='Enter your name...'
               onChange={(event) => {
                 setUsername(event.target.value);
               }}
